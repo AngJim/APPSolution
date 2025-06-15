e752c3675e2815fc697ba7e6c7d95c2e2fd8414f
Andrea Alume <andrea.alume@edu-its.it>
2025-06-15 17:27:25 +0200

    Modificato lo stato del db

--- rifugio-del-cuore/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO animali (nome, specie, razza, eta, mese, genere, taglia, stato, descrizione)
-VALUES ('Luna', 'Cane', 'Labrador', 3, 4, 'Femmina', 'Media', 'Disponibile', 'Cane giocherellone e affettuoso.');
