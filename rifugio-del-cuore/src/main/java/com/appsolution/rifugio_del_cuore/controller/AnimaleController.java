7ec90c87e94ca9d301b58216f9c2be9c1a0f433a
Andrea Alume <andrea.alume@edu-its.it>
2025-06-17 21:46:03 +0200

    Aggiunto metodo elimina

--- rifugio-del-cuore/src/main/java/com/appsolution/rifugio_del_cuore/controller/AnimaleController.java
+++ rifugio-del-cuore/src/main/java/com/appsolution/rifugio_del_cuore/controller/AnimaleController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.appsolution.rifugio_del_cuore.model.Animale;
@@ -33,5 +35,11 @@ public class AnimaleController {
 
         model.addAttribute("animali", animali);
         return "animali";
+    }
+
+    @PostMapping("/animali/elimina/{id}")
+    public String eliminaAnimale(@PathVariable Integer id) {
+        animaleRepository.deleteById(id);
+        return "redirect:/animali";
     }
 }
