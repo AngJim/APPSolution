479966f6f8eee13589c8559da6474c74b7775f52
Andrea Alume <andrea.alume@edu-its.it>
2025-06-23 20:20:32 +0200

    Inizializzato e configurato il progetto per RESTful

---
+++ rifugiodelcuore/src/main/java/com/appsolution/rifugiodelcuore/RifugiodelcuoreApplication.java
@@ -0,0 +1,13 @@
+package com.appsolution.rifugiodelcuore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifugiodelcuoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifugiodelcuoreApplication.class, args);
+	}
+
+}
---
+++ rifugiodelcuore/src/test/java/com/appsolution/rifugiodelcuore/RifugiodelcuoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsolution.rifugiodelcuore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifugiodelcuoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
