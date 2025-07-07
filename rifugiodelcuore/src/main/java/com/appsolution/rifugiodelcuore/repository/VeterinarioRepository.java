b14ac9ae89c220a238ae8a9153d69367ca61ee78
AngJim <angelo.jimenez@edu-its.it>
2025-07-07 23:59:20 +0200

    creato vari metodi: - filtro per nome e cognome - controllo Codice Fiscale esistente - controllo Email esistente

--- rifugiodelcuore/src/main/java/com/appsolution/rifugiodelcuore/repository/VeterinarioRepository.java
+++ rifugiodelcuore/src/main/java/com/appsolution/rifugiodelcuore/repository/VeterinarioRepository.java
@@ -1,9 +1,17 @@
 package com.appsolution.rifugiodelcuore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.appsolution.rifugiodelcuore.model.Veterinario;
 
 public interface VeterinarioRepository extends JpaRepository<Veterinario, Integer> {
     
+    @Query("SELECT v FROM Veterinario v WHERE " +
+           "LOWER(v.nome) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+           "LOWER(v.cognome) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Veterinario> searchByquery(@Param("query") String query);
 }
