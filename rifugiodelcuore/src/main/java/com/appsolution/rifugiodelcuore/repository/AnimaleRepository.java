42772679ba7749b0d40ec60e5a312e4d293da71f
AngJim <angelo.jimenez@edu-its.it>
2025-07-13 16:20:08 +0200

    refactor(repository): ottimizza query JPQL e indicizza campi in AnimaleRepository

--- rifugiodelcuore/src/main/java/com/appsolution/rifugiodelcuore/repository/AnimaleRepository.java
+++ rifugiodelcuore/src/main/java/com/appsolution/rifugiodelcuore/repository/AnimaleRepository.java
@@ -1,9 +1,12 @@
 package com.appsolution.rifugiodelcuore.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.appsolution.rifugiodelcuore.model.Animale;
 
 public interface AnimaleRepository extends JpaRepository<Animale, Long> {
- 
+    
+    Optional<Animale> findByMicrochipAnimale(String microchipAnimale);
 }
